9857a61a415dfb48ca0fc71da6ab12ecb006a10a
Rendon Elsesser <rendonelsesser@gmail.com>
2018-09-04 21:43:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
 function dwarfRollCall(dwarves) {
+  var dwarflist = []
+  for(var i=0 ; i<dwarves.length ; i++){
+  dwarflist.push (`${i+1}. ${dwarves[i]} `)
+  }
+  return dwarflist.join("")
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var shout = []
+  for (var i=0; i<planeteerCalls.length; i++)
+  shout.push(planeteerCalls[i].toUpperCase() + "!") 
 }
+return shout
 
 function longPlaneteerCalls(words) {
 }
